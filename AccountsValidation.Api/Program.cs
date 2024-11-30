cd52d9f9c4a2d96dc588fe1815e76d16832118c3
maksimoreo <maximthebest@outlook.com>
2024-12-01 00:28:59 +0200

    Add validation

--- AccountsValidation.Api/Program.cs
+++ AccountsValidation.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,84 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapPost("/validate", ValidateAccountsFile).WithName("GetWeatherForecast").DisableAntiforgery();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+static Results<Ok<ValidFileResponse>, BadRequest<InvalidFileResponse>> ValidateAccountsFile(
+    IFormFile file
+)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    using var reader = new StreamReader(file.OpenReadStream());
+    var invalidLines = AccountValidator.ValidateStream(reader);
+
+    if (invalidLines.Any())
+    {
+        var response = new InvalidFileResponse(FileValid: false, InvalidLines: invalidLines);
+        return TypedResults.BadRequest(response);
+    }
+
+    return TypedResults.Ok(new ValidFileResponse());
+}
+
+public record ValidFileResponse(bool FileValid = true);
+
+public record InvalidFileResponse(bool FileValid, IList<string> InvalidLines);
+
+public record Account(string Number, string Name);
+
+class AccountValidator
+{
+    public static IList<string> ValidateStream(StreamReader inputStream)
+    {
+        List<string> invalidLines = [];
+
+        for (int i = 0; inputStream.Peek() > 0; i++)
+        {
+            var line = inputStream.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(line))
+                break;
+
+            var accountEntry = ParseAccountEntry(line);
+
+            var invalidFields = ValidateAccountEntry(accountEntry);
+
+            if (invalidFields.Any())
+            {
+                var invalidFieldsSerialized = string.Join(", ", invalidFields);
+                var accountEntrySerialized = $"{accountEntry.Name} {accountEntry.Number}";
+                var lineIndex = i + 1;
+
+                var lineExplanation =
+                    $"{invalidFieldsSerialized} - not valid for {lineIndex} line '{accountEntrySerialized}'";
+
+                invalidLines.Add(lineExplanation);
+            }
+        }
+
+        return invalidLines;
+    }
+
+    public static Account ParseAccountEntry(string input)
+    {
+        var parts = input.Split(';');
+        var number = parts[0].Trim();
+        var name = parts[1].Trim();
+        var account = new Account(Number: number, Name: name);
+
+        return account;
+    }
+
+    public static IEnumerable<string> ValidateAccountEntry(Account accountEntry)
+    {
+        HashSet<string> invalidFields = [];
+
+        if (!char.IsUpper(accountEntry.Name[0]))
+        {
+            invalidFields.Add("Account name");
+        }
+
+        return invalidFields;
+    }
 }
