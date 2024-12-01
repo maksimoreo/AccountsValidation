bfc4139f4c0b824dedc1d2b332ca2903643f74b9
maksimoreo <maximthebest@outlook.com>
2024-12-02 00:02:58 +0200

    Remove auto generated code

--- AccountsValidation.Api/Program.cs
+++ AccountsValidation.Api/Program.cs
@@ -2,14 +2,11 @@ using AccountsValidation.Api.ValidationEndpoint;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,7 +16,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapPost("/validate", ValidationEndpoint.Validate)
-    .WithName("GetWeatherForecast")
+    .WithName("Validate accounts file")
     .DisableAntiforgery();
 
 app.Run();
